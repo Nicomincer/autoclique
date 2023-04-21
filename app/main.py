ecaff51264c0253b1621406668bf5df5be1a5e4d
Vésper <nicolasalencar100@gmail.com>
2023-04-21 14:07:54 -0300

    versão 0.2

--- app/main.py
+++ app/main.py
@@ -1,31 +1,42 @@
-import time
 import pyautogui 
 import tkinter 
-
+import keyboard
+import threading
+from time import time, sleep
 
 
 class Aplicação():
-     x = 0
-     def __init__(self):
-          self.valor = False
-          self.janela = tkinter.Tk()
-          self.janela.geometry("300x50")
-          self.botao_iniciar = tkinter.Button(self.janela, text="START", command=self.clicar, width=20)
-          self.botao_iniciar.place(relx=0.01, rely=0.5)
-          self.botao_parar = tkinter.Button(self.janela, text="STOP", command='', width=20)
-          self.botao_parar.place(relx=0.5, rely=0.5)
-          self.janela.mainloop()
-     def clicar(self):
-          self.valor = True
-          self.passado = time.localtime()[5]
-          self.atual = time.localtime()[5]
-          while self.valor:
-               pyautogui.click()
-               self.janela.mainloop()
-     def parar(self):
-          print('Parou')
-          
+    def __init__(self):
+        self.rodando = False
+        self.janela = tkinter.Tk()
+        self.janela.geometry("300x50")
+        self.janela.title("Autoclick")
+        self.botao_iniciar = tkinter.Button(self.janela, text="START", command=self.clicar, width=20)
+        self.botao_iniciar.place(relx=0.01, rely=0.5)
+        self.botao_parar = tkinter.Button(self.janela, text="STOP", command=self.parar, width=20)
+        self.botao_parar.place(relx=0.5, rely=0.5)
+        self.janela.mainloop()
 
+    def clicar(self):
+        if self.rodando:
+            return
+        self.rodando = True
+        th_loop_click = threading.Thread(target=self.loop_click)
+        th_loop_click.start()
+
+    def loop_click(self):        
+        tempo_limite = time() + 30
+        while time() < tempo_limite and self.rodando:
+               sleep(0.05)
+               pyautogui.click()
+               if keyboard.is_pressed("n"):
+                    self.parar()
+          
+        self.rodando = False
+
+    def parar(self):
+        self.rodando = False
+          
 
 if __name__ == "__main__":
      app = Aplicação()
